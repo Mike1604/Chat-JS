4ec32f8cbc41bfd4e2fa4af003c11049a38f0c86
Miguel Flores <84428383+Mike1604@users.noreply.github.com>
2022-10-09 14:52:15 -0500

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,6 +1,6 @@
 //Connection
 // Make connection
-var socket = io.connect('http://localhost:3000');
+var socket = io.connect('http://localhost:4000');
 
 // Query DOM
 var message = document.getElementById('message'),
